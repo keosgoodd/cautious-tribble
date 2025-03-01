60fb7c764799a9334e318569ddd322a0d570012b
keosgoodd <dcastillom.59@hotmail.com>
2025-02-28 23:30:00 -0500

    Create script_info.lua

---
+++ script_info.lua
@@ -0,0 +1,10 @@
+local info = {
+  discord = "https://discord.gg/6bzwCeH6jV";
+  status = "enabled";
+  announcement = "";
+  support_discord = {
+    strucid = "xiba#5806";
+    da_hood = "flash#1337";
+  }
+}
+return info
